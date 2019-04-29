ebd2520badb18bc5e0cca91b8a4daa0e08622d0a
Roberto Lima <robertolima@MacBook-Pro-de-Roberto.local>
2019-04-29 09:22:05 -0300

    v1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+Java application with list of rented rooms
+
+**Author: Roberto Lima**
---
+++ list/src/app/Program.java
@@ -0,0 +1,40 @@
+package app;
+
+import java.text.ParseException;
+import java.util.Scanner;
+import entities.Rent;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+
+		Scanner sc = new Scanner(System.in);
+		Rent[] vect = new Rent[10];
+
+		System.out.print("How many rooms will be rented? ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println();
+			System.out.println("Rent #" + i + ":");
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			vect[room] = new Rent(name, email);
+		}
+
+		System.out.println();
+		System.out.println("Busy rooms:");
+		for (int i = 0; i < 10; i++) {
+			if (vect[i] != null) {
+				System.out.println(i + ": " + vect[i]);
+			}
+		}
+
+		sc.close();
+	}
+}
